836752745034f114a7c88a61ae55edc6d34dabf5
xknat <kevin.natte@gmail.com>
2020-09-01 17:00:45 +0200

    woho it's alive

--- src/main/java/com/extendedclip/papi/expansion/skinsrestorer/SkinsRestorerExpansion.java
+++ src/main/java/com/extendedclip/papi/expansion/skinsrestorer/SkinsRestorerExpansion.java
@@ -1,30 +1,29 @@
 package com.extendedclip.papi.expansion.skinsrestorer;
 
+import me.clip.deluxetags.DeluxeTags;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
-
+import org.bukkit.plugin.java.JavaPlugin;
 import skinsrestorer.bukkit.SkinsRestorer;
-import skinsrestorer.shared.exception.SkinRequestException;
-import me.clip.deluxetags.DeluxeTags;
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import skinsrestorer.bukkit.SkinsRestorerBukkitAPI;
 
 public class SkinsRestorerExpansion extends PlaceholderExpansion {
+    private final String VERSION = getClass().getPackage().getImplementationVersion();
     private DeluxeTags deluxeTags;
     private SkinsRestorer skinsRestorer;
+    private SkinsRestorerBukkitAPI skinsRestorerBukkitAPI;
 
-    private final String VERSION = getClass().getPackage().getImplementationVersion();
+    public SkinsRestorerExpansion() {
+    }
 
     public boolean canRegister() {
         return (Bukkit.getPluginManager().getPlugin("SkinsRestorer") != null);
-        // might not be needed
-        /*if (!Bukkit.getPluginManager().isPluginEnabled(getRequiredPlugin())) { return false; }
-        deluxeTags = (DeluxeTags) Bukkit.getPluginManager().getPlugin(getRequiredPlugin());
-        return super.register() && deluxeTags != null; */
     }
 
     public boolean register() {
-        this.skinsRestorer = (SkinsRestorer)Bukkit.getPluginManager().getPlugin("SkinsRestorer");
+        this.skinsRestorer = (SkinsRestorer) Bukkit.getPluginManager().getPlugin("SkinsRestorer");
         if (this.skinsRestorer != null)
             return super.register();
         return false;
@@ -43,45 +42,44 @@ public class SkinsRestorerExpansion extends PlaceholderExpansion {
     }
 
     //dunno what this is
-    /*public String getRequiredPlugin() {
+    public String getRequiredPlugin() {
         return "SkinsRestorer";
-    }*/
-
+    }
 
     /**
      * This method is called when a placeholder is used and maches the set
      * {@link #getIdentifier() identifier}
      *
-     * @param  offlinePlayer
-     *         The player to parse placeholders for
-     * @param  params
-     *         The part after the identifier ({@code %identifier_params%})
-     *
+     * @param offlinePlayer The player to parse placeholders for
+     * @param params        The part after the identifier ({@code %identifier_params%})
      * @return Possible-null String
      */
     @Override
     public String onRequest(OfflinePlayer offlinePlayer, String params) {
-        if (params.equals("test")) { return "success"; }
-        if (offlinePlayer == null || !offlinePlayer.isOnline()) { return "player is not online"; }
+        if (params.equals("test")) {
+            return "success";
+        }
+        if (offlinePlayer == null || !offlinePlayer.isOnline()) {
+            return "player is not online";
+        }
 
+        skinsRestorer = JavaPlugin.getPlugin(SkinsRestorer.class);
+        skinsRestorerBukkitAPI = skinsRestorer.getSkinsRestorerBukkitAPI();
         Player player = offlinePlayer.getPlayer();
+
+        String p = player.getName();
+
         switch (params) {
-            case "name":
-                return player.getName();
-                
-            case "display_name":
-                return player.getDisplayName();
-                
-            case "gamemode":
-                return player.getGameMode().name();
-                
-            case "health":
-                return Double.toString(player.getHealth());
-                
-            case "tag_menu_name":
-                String name = deluxeTags.getGuiOptions().getMenuName();
-                return name != null ? name : "";
-                
+            case "getSkinName":
+                String name = skinsRestorerBukkitAPI.getSkinName(p);
+                if (name != null) {
+                    if (name.contains(" "))
+                        return "Url_skin";
+                    return name;
+                } else {
+                    return p;
+                }
+
             default:
                 return null;
         }
